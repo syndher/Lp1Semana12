a745113e6aaeddb4bb33f5f49a029f8d995956a1
Syndher <miguelfilipe.dev@gmail.com>
2025-05-22 21:58:42 +0100

    HowManyOfType implemented

---
+++ HowManyOfThisType/Checker.cs
@@ -0,0 +1,22 @@
+﻿namespace HowManyOfThisType
+{
+
+    public static class Checker
+    {
+        public static int HowManyOfType<T>(IEnumerable<object> items)
+        {
+            int count = 0;
+
+
+            foreach (object item in items)
+            {
+                if (item is T)
+                {
+                    count++;
+                }
+
+            }
+            return count;
+        }
+    }
+}
