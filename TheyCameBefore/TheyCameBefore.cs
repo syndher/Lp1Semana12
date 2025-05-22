ec865fcd3010d07053fe3035ea774cc356cfa4df
Syndher <miguelfilipe.dev@gmail.com>
2025-05-22 22:10:54 +0100

    TheyCameBefore implemented

---
+++ TheyCameBefore/TheyCameBefore.cs
@@ -0,0 +1,19 @@
+﻿
+namespace TheyCameBefore
+{
+
+    public static class BeforeUtils
+    {
+        public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> items, T after)
+        where T : struct, IComparable<T>
+        {
+            foreach (T item in items)
+            {
+                if (item.CompareTo(after) < 0)
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
