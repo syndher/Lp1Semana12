a5d62c98f3697293575a4be18acf6a289566b223
Syndher <miguelfilipe.dev@gmail.com>
2025-05-22 22:16:17 +0100

    Guarda3 has variables

---
+++ MyGenericClass/Guarda3.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MyGenericClass
+{
+
+    public class Guarda3<T>
+    {
+        private T placeholder1;
+        private T placeholder2;
+        private T placeholder3;
+        public Guarda3()
+        {
+            placeholder1 = default(T);
+            placeholder2 = default(T);
+            placeholder3 = default(T);
+        }
+    }
+}
