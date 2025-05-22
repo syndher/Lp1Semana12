4f6e8cac8a83566a1939962712d4b949cdc88acd
Syndher <miguelfilipe.dev@gmail.com>
2025-05-22 22:24:54 +0100

    Guarda3 implemented

--- MyGenericClass/Guarda3.cs
+++ MyGenericClass/Guarda3.cs
@@ -4,14 +4,42 @@ namespace MyGenericClass
 
     public class Guarda3<T>
     {
+        private T placeholder0;
         private T placeholder1;
         private T placeholder2;
-        private T placeholder3;
         public Guarda3()
         {
-            placeholder1 = default(T);
-            placeholder2 = default(T);
-            placeholder3 = default(T);
+            placeholder0 = default;
+            placeholder1 = default;
+            placeholder2 = default;
+        }
+        public T GetItem(int i)
+        {
+            switch (i)
+            {
+                case 0: return placeholder0;
+                case 1: return placeholder1;
+                case 2: return placeholder2;
+                default: throw new IndexOutOfRangeException();
+            }
+            
+        }
+        public void SetItem(int i, T item)
+        {
+            switch (i)
+            {
+                case 0:
+                    placeholder0 = item;
+                    break;
+                case 1:
+                    placeholder1 = item;
+                    break;
+                case 2:
+                    placeholder2 = item;
+                    break;
+                default:
+                    throw new IndexOutOfRangeException();
+            }
         }
     }
 }
