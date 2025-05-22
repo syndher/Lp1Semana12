9589aa61868759dbfc1deed1df10d5749475f6cd
Syndher <miguelfilipe.dev@gmail.com>
2025-05-22 22:30:32 +0100

    Implemented IEnumerator in Guarda3

---
+++ MyEnumerable/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace MyEnumerable
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            Guarda3<string> g3s = new Guarda3<string>();
+            Guarda3<float> g3f = new Guarda3<float>();
+
+            g3s.SetItem(0, "Olá");
+            g3s.SetItem(2, "Adeus");
+
+            g3f.SetItem(2, 54.6f);
+
+            Console.WriteLine("Strings:");
+            foreach (string s in g3s)
+            {
+                Console.WriteLine($"    {s}");
+            }
+
+            Console.WriteLine("Floats:");
+            foreach (float f in g3f)
+            {
+                Console.WriteLine($"    {f}");
+            }
+
+            // Expected output
+
+            // Strings:
+            //     Olá
+            //
+            //     Adeus
+            // Floats:
+            //     0
+            //     0
+            //     54.6
+        }
+    }
+}
